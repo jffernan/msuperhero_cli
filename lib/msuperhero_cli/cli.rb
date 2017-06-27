0d25c768846b3c5027e5703d92ed7bf7295cfc3f
James Fernandez <jffernan@comcast.net>
2017-06-27 17:51:02 +0000

    Building scraping method for 4 attributes of superhero

--- lib/msuperhero_cli/cli.rb
+++ lib/msuperhero_cli/cli.rb
@@ -1,12 +1,17 @@
 #CLI Controller
 class MsuperheroCli::CLI
   def call
+    list_heroes
     menu
   end
 
-  def list #Greet user and list superheroes from scraping
+  def list_heroes #Greet user and list superheroes from scraping
     puts "***Welcome True Believers to this CLI app to learn about popular comic book superheroes***"
     puts "The following is a list of famous superhero characters in no particular order. 'Nuff said:"
+    @superheroes = Msuperhero::Superhero.list
+    @superheroes.each.with_index(1) do |superhero, i|
+      puts "#{i}. #{superhero.name}"
+    end
   end
 
   def menu
--- lib/msuperhero_cli/superhero.rb
+++ lib/msuperhero_cli/superhero.rb
@@ -1,7 +1,7 @@
 class Msuperhero::Superhero
   attr_accessor :name, :real_name, :powers, :bio
 
-  def self.list #scrape list of http://marvel.com/characters/list/994/top_marvel_heroes
+  def self.list #scrape list to be able to print list
       self.scrape_superheroes
   end
 
@@ -9,5 +9,15 @@ class Msuperhero::Superhero
     superheroes = [] #empty array
     superheroes << self.scrape_mwebsite
     superheroes
+  end
+
+  def self.scrape_mwebsite
+    doc = Nokogiri::HTML(open("http://marvel.com/characters/list/994/top_marvel_heroes"))
+    superhero = self.new
+    superhero.name = doc.search().text.strip
+    superhero.real_name = doc.search().text.strip
+    superhero.powers = doc.search().text.strip
+    superhero.bio = doc.search().text.strip
+    superhero
   end
 end
