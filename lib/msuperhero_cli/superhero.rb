e1e625a30c297799d9d1d1ba0ab2b601a46d7dc3
James Fernandez <jffernan@comcast.net>
2017-06-28 03:49:40 +0000

    Finished code for list method to print heroes list

--- lib/msuperhero_cli/superhero.rb
+++ lib/msuperhero_cli/superhero.rb
@@ -1,12 +1,22 @@
 class MsuperheroCli::Superhero
-  def get_page
-    Nokogiri::HTML(open("http://marvel.com/characters/list/994/top_marvel_heroes"))
+  attr_accessor :name, :url, :real_name, :power, :bio
+
+  def initialize(name=nil, url=nil)
+    @name = name
+    @url = url
   end
 
-  def make_heroes
-    self.get_page.each do
-      superhero = Superhero.new
-      superhero.name = search("a[class = 'meta-title']").text.strip
-      end
+  def self.all
+    @@all ||= scrape_heroes
+  end
+
+  def self.scrape_heroes
+    doc = Nokogiri::HTML(open("http://marvel.com/characters/list/994/top_marvel_heroes"))
+    names = doc.search("a[class = 'meta-title']")
+    names.collect{|e| new(e.text.strip, "http://marvel.com#{e.attr("href").split("?").first.strip}")}
+  end
+
+  def doc
+    @doc = Nokogiri::HTML(open(self.url))
   end
 end
