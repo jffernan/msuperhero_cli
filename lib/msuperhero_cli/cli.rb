ccc0297a5c6068e29f2d531fb5df9ebaf8d5eaa8
James Fernandez <jffernan@comcast.net>
2017-06-29 00:03:08 +0000

    Added print_hero and ending methods to cli.rb

--- lib/msuperhero_cli/cli.rb
+++ lib/msuperhero_cli/cli.rb
@@ -41,5 +41,15 @@ class MsuperheroCli::CLI
     end
   end
 
-
+  def print_hero(superhero)
+      puts "-------------- #{superhero.name} --------------"
+      puts "Real Name: #{superhero.real_name}"
+      puts "Power(s): #{superhero.power}"
+      puts "Bio: #{superhero.bio}"
+    end
+  
+    def ending
+      puts "Thank you for using my app!  Excelsior!"
+    end
+  end
 
